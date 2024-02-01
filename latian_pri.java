d8098dba7db7323364ec557c557cd1f9fce62815
azzamalkahfie <156441068+azzamalkahfie@users.noreply.github.com>
2024-02-01 09:34:40 +0700

    Add files via upload

---
+++ latian_pri.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class latian_pri {
+    public static void main(String[] args) {
+        person player = new person();
+        
+        player.setName("azzam");
+        
+        System.out.println("Nama : " + player.getName());
+        System.out.println("");
+    }
+}
---
+++ person.java
@@ -0,0 +1,18 @@
+package inheritance;
+
+public class person {
+    private String Name;
+
+    static void StringName(String azzam) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+public void setName(String name){
+this.Name = name;
+}
+
+String getName(){
+return Name;
+}
+}
